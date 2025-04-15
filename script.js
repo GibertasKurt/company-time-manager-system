2ed3d7644a47d059fc4659cacaf2d915f883ee38
OneOneOneZeroSixFiveEight <1110658@ub.edu.ph>
2025-04-15 15:29:43 +0800

    Add initial HTML structure, CSS styles, and JavaScript TODOs for Company Time Manager

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+
+    TO DO:
+    A Company has many Departments, and each Department has many Employees.
+    Add Filter to the search function (Day, Time of Clock in/out, Employee Name, Department Name)
+    The user (employee) can clock in/out.
+    The user (employee) can see their clock in/out history.
+    If the user is already login and tries to login again, show a message "You are already logged in".
+    The user's current session should be saved in local storage.
+
+*/
