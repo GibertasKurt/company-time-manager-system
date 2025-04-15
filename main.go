df055050a5703e244d31a1dfa556270a33c164d0
OneOneOneZeroSixFiveEight <1110658@ub.edu.ph>
2025-04-15 17:32:18 +0800

    Remove inline registration for Employees relationship in Departments model

--- main.go
+++ main.go
@@ -9,9 +9,6 @@ func main() {
 	uadmin.Register(
 		models.Departments{},
 	)
-	uadmin.RegisterInlines(models.Departments{}, map[string]string{
-		"uadmin.User": "EmployeesID",
-	})
 	uadmin.SiteName = "Company Time Management System"
 	uadmin.StartServer()
 }
