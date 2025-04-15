813b02ddc7c168a45e212859284f9a99b0de0333
OneOneOneZeroSixFiveEight <1110658@ub.edu.ph>
2025-04-15 17:11:06 +0800

    Implement departments model, enhance login functionality, and update UI components

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/uadmin/uadmin"
+import (
+	"github.com/kurt/company-time-manager-system/models"
+	"github.com/uadmin/uadmin"
+)
 
 func main() {
+	uadmin.Register(
+		models.Departments{},
+	)
+	uadmin.RegisterInlines(models.Departments{}, map[string]string{
+		"uadmin.User": "DepartmentsID",
+	})
 	uadmin.SiteName = "Company Time Management System"
 	uadmin.StartServer()
 }
---
+++ models/departments.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/uadmin/uadmin"
+
+type Departments struct {
+	uadmin.Model
+	Name        string
+	Description string
+}
